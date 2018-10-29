fe6cc65d8ad4132d5bc1e5ccb202fdc1bb0df0a6
Alex Sanchez <lexsanchez98@gmail.com>
2018-10-29 10:51:58 -0400

    fixed bug with piedmont army

--- Frontend/MapModel.js
+++ Frontend/MapModel.js
@@ -95,6 +95,143 @@ MapModel.prototype.loadGameState = function() {
 			nation: Nation.RUSSIA,
 			coast: 'SC'
 		}
+		/*,
+		'Prussia': {
+			type: 'army',
+			nation: Nation.GERMANY
+		},
+		'Silesia': {
+			type: 'army',
+			nation: Nation.GERMANY
+		},
+		'Ruhr': {
+			type: 'army',
+			nation: Nation.GERMANY
+		},
+		'Wales': {
+			type: 'army',
+			nation: Nation.ENGLAND
+		},
+		'Yorkshire': {
+			type: 'fleet',
+			nation: Nation.ENGLAND
+		},
+		'Clyde': {
+			type: 'army',
+			nation: Nation.ENGLAND
+		},
+		'Burgundy': {
+			type: 'army',
+			nation: Nation.FRANCE
+		},
+		'Gascony': {
+			type: 'fleet',
+			nation: Nation.FRANCE
+		},
+		'Picardy': {
+			type: 'army',
+			nation: Nation.FRANCE
+		},
+		'Piedmont': {
+			type: 'fleet',
+			nation: Nation.ITALY
+		},
+		'Tuscany': {
+			type: 'army',
+			nation: Nation.ITALY
+		},
+		'Apulia': {
+			type: 'army',
+			nation: Nation.ITALY
+		},
+		'Tyrolia': {
+			type: 'army',
+			nation: Nation.AUSTRIA
+		},
+		'Bohemia': {
+			type: 'army',
+			nation: Nation.AUSTRIA
+		},
+		'Galicia': {
+			type: 'army',
+			nation: Nation.AUSTRIA
+		},
+		'Armenia': {
+			type: 'fleet',
+			nation: Nation.TURKEY
+		},
+		'Syria': {
+			type: 'army',
+			nation: Nation.TURKEY
+		},
+		'Rumania': {
+			type: 'army',
+			nation: Nation.TURKEY
+		},
+		'Ukraine': {
+			type: 'army',
+			nation: Nation.RUSSIA
+		},
+		'Livonia': {
+			type: 'fleet',
+			nation: Nation.RUSSIA
+		},
+		'Serbia': {
+			type: 'army',
+			nation: Nation.RUSSIA
+		},
+		'Finland': {
+			type: 'army',
+			nation: Nation.RUSSIA
+		},
+		'Albania': {
+			type: 'fleet',
+			nation: Nation.AUSTRIA
+		},
+		'Greece': {
+			type: 'fleet',
+			nation: Nation.AUSTRIA
+		},
+		'Bulgaria': {
+			type: 'army',
+			nation: Nation.TURKEY,
+		},
+		'Tunis': {
+			type: 'army',
+			nation: Nation.TURKEY
+		},
+		'NorthAfrica': {
+			type: 'army',
+			nation: Nation.TURKEY
+		},
+		'Portugal': {
+			type: 'fleet',
+			nation: Nation.RUSSIA
+		},
+		'Belgium': {
+			type: 'fleet',
+			nation: Nation.RUSSIA
+		},
+		'Holland': {
+			type: 'fleet',
+			nation: Nation.RUSSIA
+		},
+		'Norway': {
+			type: 'army',
+			nation: Nation.RUSSIA
+		},
+		'Sweden': {
+			type: 'army',
+			nation: Nation.RUSSIA
+		},
+		'Spain': {
+			type: 'army',
+			nation: Nation.RUSSIA
+		},
+		'Denmark': {
+			type: 'army',
+			nation: Nation.RUSSIA
+		}*/
 	}
 	this.territories = {
 		'Kiel': Nation.GERMANY,
