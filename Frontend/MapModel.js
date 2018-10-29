da7d8249a3a165274ddf987d2bc3f123d4dc55d1
Alex Sanchez <lexsanchez98@gmail.com>
2018-10-29 13:40:48 -0400

    cleaned up code, made mouse cursor change based off if territory had unit, or if territory is invalid

--- Frontend/MapModel.js
+++ Frontend/MapModel.js
@@ -5,6 +5,50 @@ function MapModel(presenter) {
 
 MapModel.prototype.loadGameState = function() {
 	// get from server
+	this.territories = {
+		'Kiel': Nation.GERMANY,
+		'Berlin': Nation.GERMANY,
+		'Munich': Nation.GERMANY,
+		'Prussia': Nation.GERMANY,
+		'Silesia': Nation.GERMANY,
+		'Ruhr': Nation.GERMANY,
+		'Warsaw': Nation.RUSSIA,
+		'Livonia': Nation.RUSSIA,
+		'Moscow': Nation.RUSSIA,
+		'StPetersburg': Nation.RUSSIA,
+		'Sevastopol': Nation.RUSSIA,
+		'Ukraine': Nation.RUSSIA,
+		'Ankara': Nation.TURKEY,
+		'Smyrna': Nation.TURKEY,
+		'Syria': Nation.TURKEY,
+		'Armenia': Nation.TURKEY,
+		'Constantinople': Nation.TURKEY,
+		'Tyrolia': Nation.AUSTRIA,
+		'Budapest': Nation.AUSTRIA,
+		'Galicia': Nation.AUSTRIA,
+		'Bohemia': Nation.AUSTRIA,
+		'Vienna': Nation.AUSTRIA,
+		'Trieste': Nation.AUSTRIA,
+		'Venice': Nation.ITALY,
+		'Apulia': Nation.ITALY,
+		'Piedmont': Nation.ITALY,
+		'Tuscany': Nation.ITALY,
+		'Rome': Nation.ITALY,
+		'Naples': Nation.ITALY,
+		'Marseilles': Nation.FRANCE,
+		'Burgundy': Nation.FRANCE,
+		'Paris': Nation.FRANCE,
+		'Gascony': Nation.FRANCE,
+		'Picardy': Nation.FRANCE,
+		'Brest': Nation.FRANCE,
+		'London': Nation.ENGLAND,
+		'Yorkshire': Nation.ENGLAND,
+		'Wales': Nation.ENGLAND,
+		'Liverpool': Nation.ENGLAND,
+		'Clyde': Nation.ENGLAND,
+		'Edinburgh': Nation.ENGLAND
+	}
+	
 	this.units = {
 		'Kiel': {
 			type: 'fleet',
@@ -233,12 +277,9 @@ MapModel.prototype.loadGameState = function() {
 			nation: Nation.RUSSIA
 		}*/
 	}
-	this.territories = {
-		'Kiel': Nation.GERMANY,
-		'Berlin': Nation.GERMANY
-	}
+	
 	this.moves = [];
-	this.presenter.update(this.units);
+	this.presenter.update(this.territories, this.units);
 }
 
 MapModel.prototype.getUnitAt = function(territory) {
